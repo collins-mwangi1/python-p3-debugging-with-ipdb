682a06c70d76f57819c8d0af8defe6ef40d6a354
collins-mwangi1 <collins.mwangi1@student.moringaschool.com>
2024-05-23 10:31:21 +0300

    lab done

--- lib/ipdb_debugging.py
+++ lib/ipdb_debugging.py
@@ -1,7 +1,5 @@
 #!/usr/bin/env python3
 
-import ipdb
-
 def plus_two(num):
-    num + 2
-    return num
+    num += 2  # Increment num by 2 and assign the result back to num
+    return num  # Return the updated value of num
